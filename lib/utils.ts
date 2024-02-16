d77a2045a13f32f3507b58f8f78ed500e6705afb
Deepraj <deepraj21.bera@gmail.com>
2024-02-16 18:15:57 +0530

    Updated utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,39 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import { 
+  Camera, 
+  Color, 
+  Layer, 
+  LayerType, 
+  PathLayer, 
+  Point, 
+  Side, 
+  XYWH
+} from "@/types/canvas";
+
+const COLORS = [
+  "#DC2626", 
+  "#D97706", 
+  "#059669", 
+  "#7C3AED", 
+  "#DB2777"
+];
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
+};
+
+export function pointerEventToCanvasPoint(
+  e: React.PointerEvent,
+  camera: Camera,
+) {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+};
